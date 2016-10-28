8891ca2068650120c953154b496aa5129c6a485b
Choudhary, Arpit <AChoudhary@mast.com>
2016-10-28 16:44:23 +0530

    project setup

---
+++ swipedismisslayout/src/main/java/com/viewgroup/sample/SwipeDismissLayout.java
@@ -0,0 +1,28 @@
+package com.viewgroup.sample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by arpit on 10/28/16.
+ */
+public class SwipeDismissLayout extends ViewGroup{
+
+    public SwipeDismissLayout(Context context) {
+        super(context);
+    }
+
+    public SwipeDismissLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwipeDismissLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
